c73b6d5229da7dc63ca5147fa83cc92a95b13e7f
El Ouissi Fares <fares.elouissi@esprit.tn>
2024-03-27 23:34:49 +0100

    change label to bottom when user is logged in

--- src/Components/showEnvironement/ShowEnv.tsx
+++ src/Components/showEnvironement/ShowEnv.tsx
@@ -3,12 +3,19 @@ import React, { useState, useEffect, FC } from 'react';
 
 import generatedGitInfo from '../../generatedGitInfo.json';
 import { activeEnv } from '../../util/constant';
+import { useAuth } from '../../Contexts/AuthContext';
 
 interface ShowEnvProps {
     
 }
  
 const ShowEnv: FC<ShowEnvProps> = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const { user } = useAuth();
+  useEffect(() => {
+    user?setLoggedIn(true):setLoggedIn(false)
+  }, [user])
+  
     const getEnv =() => {
         return activeEnv();
       }
@@ -16,7 +23,7 @@ const ShowEnv: FC<ShowEnvProps> = () => {
     return (
       <div>
         <div className='env'>{getEnv()}</div>
-        <div className='branch'> Current Git Branch: {generatedGitInfo.gitBranch}</div>
+        <div  className={loggedIn?`branch bottom-0 `:`branch`}> Current Git Branch: {generatedGitInfo.gitBranch}</div>
       </div>
     );
   };
