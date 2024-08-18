47c0699ba4683968a3804a811d0708a701561618
Fares_EL_Ouissi <frs@wevioo.com>
2024-08-18 05:52:50 +0100

    [SCRUM-44 change table , change order details table]

--- src/Components/AdminDashboard/AdminDashboard.tsx
+++ src/Components/AdminDashboard/AdminDashboard.tsx
@@ -91,7 +91,7 @@ const AdminDashboard: FC<AdminDashboardProps> = () => {
 
 
   return (
-    <AdminDashboardWrapper>
+    <AdminDashboardWrapper className="adminDashboard">
       <ToastContainer />
       <WeviooNavbar />
       <TopBarLinks shrunk={isNavbarShrunk}>
@@ -120,86 +120,17 @@ const AdminDashboard: FC<AdminDashboardProps> = () => {
           Orders
         </Links>
       </TopBarLinks>
-      <div className="adminDashboard pt-5">
+      
         {loading ? (
           <WeviooSpinner></WeviooSpinner>
         ) : (
           <>
-            <MainDashboard>
+           
               {view === "userList" ? (
-               /*  <div  className="pb-5 d-flex align-items-start justify-content-center mt-5 w-100 ">
-                  <UpdateRoleModal
-                    fetchUsers={fetchUsers}
-                    isOpen={isUpdateRoleModalOpen}
-                    onClose={handleCloseModal}
-                    user={userToUpdateRole ? userToUpdateRole[0] : undefined}
-                  ></UpdateRoleModal>
-                  <table className="text-center userListTable">
-                    <thead>
-                      <tr>
-                        <th>Actions</th>
-                        <th>Role</th>
-                        <th>change Role</th>
-                        <th>name</th>
-                        <th>surname</th>
-                      </tr>
-                    </thead>
-                    {updatingUser ? (
-                      <tbody className="emptyTable">
-                        <tr>
-                          <td colSpan={5}>
-                            {" "}
-                            <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-                          </td>{" "}
-                        </tr>
-                      </tbody>
-                    ) : (
-                      <tbody>
-                        {users.map((user: User) => (
-                          <tr key={user.userId}>
-                            <td>
-                              <Pen
-                                color="white"
-                                className="me-3 clickable"
-                              ></Pen>
-                              <Trash
-                                className="clickable"
-                                onClick={() => deleteUser(user.userId)}
-                                color="red"
-                              >
-                                {" "}
-                              </Trash>
-                            </td>
-                            <td>{user.userRole}</td>
-                            <td>
-                              <ArrowClockwise
-                                className="clickable"
-                                onClick={() => UpdateRole(user.userId)}
-                              />
-                            </td>
-                            <td>{user.userFirstName}</td>
-                            <td>{user.userLastName}</td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    )}
-
-                    <tfoot>
-                      {" "}
-                      <tr>
-                        <th>Actions</th>
-                        <th>Role</th>
-                        <th>change Role</th>
-                        <th>name</th>
-                        <th>surname</th>
-                      </tr>
-                    </tfoot>
-                  </table>
-                </div> */
                 <div
           className=" d-flex flex-column justify-content-center align-items-center w-100 py-2 px-3"
         >
-                <div className=" rounded-3 w-75 bg-light p-2 ">
+                <div className=" rounded-3 w-75 bg-light p-2 mt-5 ">
 
                   <ManageUserTable data={users}></ManageUserTable>
                 </div>
@@ -212,10 +143,10 @@ const AdminDashboard: FC<AdminDashboardProps> = () => {
               )}
               {/*     </div> */}
               {/*  {view === "userList" ? (<div ><Table  data={users} itemsPerPage={2}></Table></div>) */}
-            </MainDashboard>
+           
           </>
         )}
-      </div>
+      
     </AdminDashboardWrapper>
   );
 };
